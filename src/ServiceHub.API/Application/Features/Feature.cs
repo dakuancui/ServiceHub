304814209d37bf900273544f035e41b3f9613163
DakuanC <Dakuan.Cui@asb.co.nz>
2023-08-25 11:54:32 +1200

    Feature base class implementation.

---
+++ src/ServiceHub.API/Application/Features/Feature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ServiceHub.API.Application.Consumers;
+using ServiceHub.API.Application.Triggers;
+
+namespace ServiceHub.API.Application.Features
+{
+	public abstract class Feature : IFeature
+	{
+		public Feature()
+		{
+		}
+
+		public abstract IEnumerable<ITrigger> Triggers { get; }
+		public abstract IEnumerable<IConsumer> Consumers { get; }
+        public abstract string Name { get; }
+		public abstract void Apply();
+    }
+}
+
